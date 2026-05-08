411793f2a08ec685961d6f9c093f7368b11c0bb7
Nash <nashvalentina@gmail.com>
2026-05-08 16:09:09 -0500

    Update bfs.hpp

--- src/bfs.hpp
+++ src/bfs.hpp
@@ -1,1 +1,19 @@
 
+#ifndef BFS_HPP
+#define BFS_HPP
+
+#include "graph.hpp"
+#include <vector>
+#include <climits>
+
+struct BFSResult {
+    std::vector<int> dist;
+    std::vector<int> parent;
+    long long nodes_explored;
+};
+
+BFSResult bfs(const Graph& g, int source_internal, int target_internal = -1);
+
+int bfs_eccentricity(const Graph& g, int source_internal, int& farthest_node);
+
+#endif
