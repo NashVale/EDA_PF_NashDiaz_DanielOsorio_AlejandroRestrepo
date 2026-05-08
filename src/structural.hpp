d244f3d59ea789b25ea8da1f1fa022e5dbf48c35
Nash <nashvalentina@gmail.com>
2026-05-08 16:13:46 -0500

    Update structural.hpp

--- src/structural.hpp
+++ src/structural.hpp
@@ -1,1 +1,23 @@
 
+#ifndef STRUCTURAL_HPP
+#define STRUCTURAL_HPP
+
+#include "graph.hpp"
+#include <string>
+
+struct GraphStats {
+    int num_nodes;
+    long long num_edges;
+    int main_component_size;
+    int num_components;
+    double avg_degree;
+    int max_degree;
+    int max_degree_node_orig;
+    int diameter_estimate;
+};
+
+GraphStats analyze_graph(const Graph& g);
+
+void save_structural_analysis(const GraphStats& s, const std::string& filename);
+
+#endif
